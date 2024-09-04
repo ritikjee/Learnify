05a85737ce3d1cd383c7caf8c33bfe561ec7b156
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2024-09-05 01:10:25 +0530

    added service function in auth service

---
+++ auth-service/src/server.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Hello from auth service' });
+});
+
+app.all('*', (req: Request, res: Response) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
